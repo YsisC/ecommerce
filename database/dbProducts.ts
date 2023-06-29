10ffa7957c3e9a87aa5e286a0f228046ab4f9ba5
YsisC <ysisclongart@gmail.com>
2023-06-29 11:50:18 -0400

    getProductsByTerm

--- database/dbProducts.ts
+++ database/dbProducts.ts
@@ -33,3 +33,19 @@ export const getAllProductSlugs  = async ():Promise<ProductSlug[]> => {
 
     return slugs;
 }
+
+export const getProductsByTerm = async ( term:string): Promise<IProduct[]> => {
+    
+    term = term.toString().toLowerCase();
+
+    await db.connect();
+    const products = await Product.find({
+        $text: { $search: term }
+    })
+    .select('title images price inStock slug -_id')
+    .lean();
+
+    await db.disconnect();
+
+    return products;
+}
--- models/index.tsx
+++
@@ -1,3 +0,0 @@
-
-
-export { default as Product } from './Product'
