53501ca311f5a64159d268e72e2bfb35977b3b44
YsisC <ysisclongart@gmail.com>
2023-07-18 17:03:05 -0400

    middleware comment

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,30 @@
-import { NextResponse, type NextRequest } from "next/server";
-import { jwt } from './utils';
+// import { NextResponse, type NextRequest } from "next/server";
+// import { jwt } from './utils';
 
-  export async function middleware(req: NextRequest) {
-    const previousPage = req.nextUrl.pathname;
+//   export async function middleware(req: NextRequest) {
+//     const previousPage = req.nextUrl.pathname;
    
-    if (previousPage.startsWith("/checkout")) {
-      const token = req.cookies.get("token")?.value;
+//     if (previousPage.startsWith("/checkout")) {
+//       const token = req.cookies.get("token")?.value;
 
-      if (!token) {
-        return NextResponse.redirect(
-          new URL(`/auth/login?p=${previousPage}`, req.url)
-        );
-      }
+//       if (!token) {
+//         return NextResponse.redirect(
+//           new URL(`/auth/login?p=${previousPage}`, req.url)
+//         );
+//       }
     
-  try {
-    await jwt.isValidToken(
-      token
+//   try {
+//     await jwt.isValidToken(
+//       token
       
-      );
-      return NextResponse.next();
-  } catch (error) {
+//       );
+//       return NextResponse.next();
+//   } catch (error) {
     
-    return NextResponse.redirect(`/auth/login?p=${previousPage}`);
-  }
-}
-  }
-export const config = {
-  matcher: ["/checkout/:path*"],
-};
+//     return NextResponse.redirect(`/auth/login?p=${previousPage}`);
+//   }
+// }
+//   }
+// export const config = {
+//   matcher: ["/checkout/:path*"],
+// };
